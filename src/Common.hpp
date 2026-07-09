7e39cac607c4c3d96e9f12dd3bacc017352aa9e5
harukyabe2 <kakarotto.3662.dragon@gmail.com>
2026-07-09 18:42:23 +0900

    Add Common.hpp

---
+++ src/Common.hpp
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+
+struct GameData
+{
+	Size boardSize = {0, 0};
+	int32 mineCount = 0;
+	Point startGridPos = {0, 0};
+	Point keyGridPos = {0, 0};
+	Point goalGridPos = {0, 0};
+};
+
+using App = SceneManager<String, GameData>;
